e7328d00ecff46473f01eca7d74c7cddbe8a489c
ChrisL <caleedev@gmail.com>
2025-12-03 16:06:25 -0500

    array functions

--- app.js
+++ app.js
@@ -49,3 +49,115 @@ Complete Exercise 4 in the space below:
 let favFood = foods[1]
 console.log(favFood);
 
+
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+foods.splice(2, 0, 'tofu') //splice will insert new values BEFORE the index provided.
+
+
+
+/*
+Exercise 6: Replace elements
+
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+Complete Exercise 6 in the space below:
+*/
+
+foods.splice(1,1,'sushi', 'cupcake')
+
+/*
+Exercise 7: Using the `slice()` method
+
+1) Use the `slice()` method to create a new array that contains 'sushi' and 
+   'cupcake'.
+
+2) Assign it to a variable named `yummy`.
+
+Complete Exercise 7 in the space below:
+*/
+
+let yummy = foods.slice(1,3) //once again slice does not include the end of the index, 
+                            // \slice(1,3) will create a new array with elements 1 and 2 but not 3\ strange
+                            //What makes this wild to me is we would have to slice an element that does not exist to slice the end of the array.
+
+console.log(yummy);
+
+
+/*
+Exercise 8: Finding an index
+
+1) Using the `indexOf()` method, find the index of the string 'tofu' in the 
+   `foods` array. 
+
+2) Assign it to a variable named `soyIdx`.
+
+Complete Exercise 8 in the space below:
+*/
+
+let soyIDx = foods.indexOf('tofu')
+console.log(soyIDx);
+
+
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+let allFoods = foods.join(" -> ");
+console.log(allFoods);
+
+
+/*
+Exercise 10: Check for an element
+
+1) Using the .includes() method, check if the `foods` array contains the string
+   'soup'.
+
+2) Assign the result to a variable called `hasSoup``.
+
+Complete Exercise 10 in the space below:
+*/
+
+let hasSoup = foods.includes('soup')
+console.log(hasSoup);
+
+
+
+/*
+Exercise 11: Odd numbers from an array
+
+1) Choose a method to iterate through the `nums` array.
+
+2) Push each odd number to a new array named `odds`.
+
+Hint: Initialize the `odds` variable to an empty array before the iteration.
+
+Complete Exercise 11 in the space below:
+*/
+
+const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
+
+let odds = [];
+nums.forEach((element) => {
+    if (element%2 !=0) {
+        odds.push(element)
+    }
+})
+
+
+console.log('Exercise 11 result:', odds);
